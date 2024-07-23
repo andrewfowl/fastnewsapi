b538b182fbc715084be03d0b642a2864456118e7
andrewfowl <56274845+andrewfowl@users.noreply.github.com>
2024-07-23 00:24:06 -0400

    Update and rename redisclient.py to redis_client.py
    logging

--- redis_client.py
+++ redis_client.py
@@ -1,16 +1,20 @@
 import aioredis
 from fastapi import FastAPI
+import logging
 import os
 
+logger = logging.getLogger(__name__)
 redis = None
 redisurl=os.getenv("REDIS_URL")
 
 async def init_redis_pool():
     global redis
     redis = await aioredis.create_redis_pool(
-        'redis://localhost', minsize=5, maxsize=10
+        redisurl, minsize=5, maxsize=10
     )
+    logger.info("Redis connection pool created")
 
 async def close_redis_pool():
     redis.close()
     await redis.wait_closed()
+    logger.info("Redis connection pool closed")
