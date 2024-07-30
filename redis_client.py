40fa119edc6aaa59ce0cf4bf74f6eb5494561434
andrewfowl <56274845+andrewfowl@users.noreply.github.com>
2024-07-30 08:49:27 -0400

    Update redis_client.py

--- redis_client.py
+++ redis_client.py
@@ -8,7 +8,7 @@ redisurl = os.getenv("REDIS_URL")
 
 async def init_redis_pool():
     global redis_client
-    redis_client = aioredis.Redis(host=redisurl)
+    redis_client = aioredis.Redis(host=redisurl, port=6379)
     logger.info("Redis connection pool created")
 
 async def close_redis_pool():
