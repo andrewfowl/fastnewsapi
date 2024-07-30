12178c9299c1bf81acf5676a62b23676994b7bcb
andrewfowl <56274845+andrewfowl@users.noreply.github.com>
2024-07-30 08:55:25 -0400

    Update redis_client.py
    logging

--- redis_client.py
+++ redis_client.py
@@ -8,10 +8,20 @@ redisurl = os.getenv("REDIS_URL")
 
 async def init_redis_pool():
     global redis_client
-    redis_client = aioredis.Redis(host=redisurl, port=6379)
-    logger.info("Redis connection pool created")
+    if redisurl is None:
+        logger.error("REDIS_URL environment variable not set")
+        return
+    try:
+        redis_client = aioredis.Redis.from_url(redisurl)
+        logger.info(f"Redis connection pool created for {redisurl}")
+    except Exception as e:
+        logger.error(f"Error creating Redis connection pool: {e}")
 
 async def close_redis_pool():
-    await redis_client.close()
-    await redis_client.connection_pool.disconnect()
-    logger.info("Redis connection pool closed")
+    global redis_client
+    if redis_client is not None:
+        await redis_client.close()
+        await redis_client.connection_pool.disconnect()
+        logger.info("Redis connection pool closed")
+    else:
+        logger.error("Redis client is not initialized")
