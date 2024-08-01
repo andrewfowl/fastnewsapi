3c3261b1b3542b60d23f1ff70549c5ceae1f7b6c
andrewfowl <56274845+andrewfowl@users.noreply.github.com>
2024-07-31 20:18:36 -0400

    Update redis_client.py

--- redis_client.py
+++ redis_client.py
@@ -3,11 +3,9 @@ import logging
 import os
 
 logger = logging.getLogger(__name__)
-redis_client = None
 redisurl = os.getenv("REDIS_PRIVATE_URL")
 
 async def init_redis_pool():
-    global redis_client
     if redisurl is None:
         logger.error("REDIS_PRIVATE_URL environment variable not set")
         return
@@ -20,7 +18,6 @@ async def init_redis_pool():
         logger.error(f"Error creating Redis connection pool: {e}")
 
 async def close_redis_pool():
-    global redis_client
     if redis_client is not None:
         await redis_client.close()
         await redis_client.connection_pool.disconnect()
