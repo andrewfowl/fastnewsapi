0c6d17c42059eef54c7f095626b85c1390dd4572
andrewfowl <56274845+andrewfowl@users.noreply.github.com>
2024-08-03 10:06:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ async def rss(
             for feed_item in feed_items
         ]
 
-        return formatted_items
+    return formatted_items
     except Exception as e:
         logger.error(f"Error fetching data from Redis: {e}")
         raise HTTPException(status_code=500, detail="Internal Server Error")
