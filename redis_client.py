76b2100bdbb1b3930e814b4edf97549075784cf2
andrewfowl <56274845+andrewfowl@users.noreply.github.com>
2024-08-03 11:30:03 -0400

    Update redis_client.py

--- redis_client.py
+++ redis_client.py
@@ -4,24 +4,21 @@ import logging
 import os
 
 logger = logging.getLogger(__name__)
-redis_url = os.getenv("REDIS_URL")
-redis_port = os.getenv("REDISPORT")
-redis_host = os.getenv("REDISHOST")
-redis_pass = os.getenv("REDIS_PASSWORD")
-redis_pool = redis.ConnectionPool(
-            host=redis_host,
-            port=redis_port,
-            password=redis_pass,
-            decode_responses=True,
-            ssl_cert_reqs=None
-        )
-redis_connection = None
 
-def init_redis_pool():
+
+async def init_redis_pool():
     global redis_connection, redis_pool
+    redis_url = os.getenv("REDIS_URL")
+    redis_port = os.getenv("REDISPORT")
+    redis_host = os.getenv("REDISHOST")
+    redis_pass = os.getenv("REDIS_PASSWORD")
     try:
+        redis_pool = redis.ConnectionPool(host=redis_host,port=redis_port,password=redis_pass,decode_responses=True,ssl_cert_reqs=None)
         redis_connection = redis.StrictRedis(connection_pool=redis_pool)
         logger.info(f"Redis client created for {redis_url}")
+        test_connection = await redis_connection.ping()
+        logger.info(f"Redis connection test: {test_connection}")
+        return redis_connection 
     except Exception as e:
         logger.error(f"Error creating Redis client: {e}")
 
