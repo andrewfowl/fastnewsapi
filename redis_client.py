7fca5a216f2ecf03a5e85dee6d11a6674887f359
andrewfowl <56274845+andrewfowl@users.noreply.github.com>
2024-08-03 09:31:52 -0400

    Update redis_client.py

--- redis_client.py
+++ redis_client.py
@@ -1,4 +1,5 @@
-import redis
+import asyncio
+import redis.asyncio as redis
 import logging
 import os
 
@@ -7,38 +8,28 @@ redis_url = os.getenv("REDIS_URL")
 redis_port = os.getenv("REDISPORT")
 redis_host = os.getenv("REDISHOST")
 redis_pass = os.getenv("REDIS_PASSWORD")
-redis_client = redis.StrictRedis(
+redis_pool = redis.ConnectionPool(
             host=redis_host,
             port=redis_port,
             password=redis_pass,
             decode_responses=True,
-            health_check_interval=2
+            ssl_cert_reqs=none
         )
-## redis_pool = redis.ConnectionPool( host=redis_host, port=redis_port, password=redis_pass, decode_responses=True, ssl_cert_reqs=none)
-## connection = redis.StrictRedis(connection_pool=redis_pool)
+redis_connection = None
 
 def init_redis_pool():
-    global redis_client
+    global redis_connection, redis_pool
     try:
-        redis_client = redis.StrictRedis(
-            host=redis_host,
-            port=redis_port,
-            password=redis_pass,
-            decode_responses=True,
-            health_check_interval=2
-        )
+        redis_connection = redis.StrictRedis(connection_pool=redis_pool)
         logger.info(f"Redis client created for {redis_url}")
-        return redis_client
     except Exception as e:
         logger.error(f"Error creating Redis client: {e}")
 
 def close_redis_pool():
-    global redis_client
-    if redis_client:
-        logger.warning("Redis client was not initialized, nothing to close")
-    else:
-        try:
-            redis_client.close()
-            logger.info("Redis connection pool closed")
-        except Exception as e:
-            logger.error(f"Error closing Redis connection pool: {e}")
+    global redis_connection, redis_pool
+    try:
+        await redis_connection.aclose()
+        await redis_pool.aclose()
+        logger.info("Redis connection pool closed")
+    except Exception as e:
+        logger.error(f"Error closing Redis connection pool: {e}")
