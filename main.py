ee87e4baab7c9f8b01e3a5a4ff58eace544e6d71
andrewfowl <56274845+andrewfowl@users.noreply.github.com>
2024-08-03 09:59:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import asyncio
 from fastapi import FastAPI, Depends, Query, HTTPException
-from redis_client import init_redis_pool, close_redis_pool, redis_client
+from redis_client import init_redis_pool, close_redis_pool
 from pagination import paginate
 from typing import List
 import logging
@@ -14,14 +15,14 @@ def startup_event():
     init_redis_pool()
 
 @app.on_event("shutdown")
-def shutdown_event():
-    close_redis_pool()
+async def shutdown_event():
+    await close_redis_pool()
 
 def get_redis():
     try:
-        return redis_client
+        return redis_client.redis_connection
     except Exception as e:
-        logger.error("Redis client not initialized when accessed")
+        logger.error(f"Redis connection not initialized when accessed. Error: {e}")
 
 def get_redis_connection(redis=Depends(get_redis)):
     try:
@@ -29,38 +30,35 @@ def get_redis_connection(redis=Depends(get_redis)):
     finally:
         pass  
 
+class News(SQLModel):
+    title: str = Field(default=None, nullable=True)
+    link: str = Field(default=None, nullable=True)
+    published: date = Field(default=None, nullable=True)
+    summary: str = Field(default=None, nullable=True)
+
 @app.get("/rss", response_model=List[str])
-def rss(
+async def rss(
     page: int = Query(1, ge=1, description="Page number"),
     page_size: int = Query(10, ge=1, le=100, description="Number of items per page"),
     redis=Depends(get_redis_connection)
 ):
     logger.info(f"Received request for page: {page}, page_size: {page_size}")
 
-    try:
-        # Get all the keys
-        keys = redis.smembers('rss_links')
-        keys = list(keys)
-        logger.info(f"Total keys: {len(keys)}")
-
-        # Paginate the keys
-        start_index = (page - 1) * page_size
-        end_index = start_index + page_size
-        paginated_keys = keys[start_index:end_index]
-
-        # Fetch the items corresponding to the paginated keys
-        feed_items = [redis.hgetall(f"rss_item:{key}") for key in paginated_keys]
-        logger.info(f"Values retrieved: {feed_items}")
-
-        # Format the items
-        formatted_items = [
+    schema = (TextField("$.title", as_name="name"), TextField("$.link", as_name="link"), TextField("$.published", as_name="published"), TextField("$.summary", as_name="summary"))
+    redis.ft().create_index(schema, definition=IndexDefinition(prefix=["news:"], index_type=IndexType.JSON))
+    q = Query("*").paging(page, page_size).sort_by("published", asc=False)
+    feed_items = []
+    feed_items = redis.ft().search(q).docs
+    logger.info(f"Values retrieved: {feed_items}")
+    # Format the items
+    formatted_items = [
             {
                 'title': item.get('title', 'No title'),
                 'link': item.get('link', 'No link'),
                 'published': item.get('published', 'No date'),
                 'summary': item.get('summary', 'No summary')
             }
-            for item in feed_items
+            for feed_item in feed_items
         ]
 
         return formatted_items
